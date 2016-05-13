8522166b0f5329d975324712d14a6bfcc83467cf
David Hovemeyer <david.hovemeyer@gmail.com>
2016-05-13 09:27:50 -0400

    minor cleanups

--- src/advgame2/grid.cljs
+++ src/advgame2/grid.cljs
@@ -1,27 +1,27 @@
 (ns advgame2.grid
-  (:require [advgame2.pos :as pos])
-  )
+  (:require [advgame2.pos :as pos]))
 
-(defn create-terrain-vec [ncol nrow genfn]
-  (vec (for [y (range 0 nrow)]
-         (vec (for [x (range 0 ncol)]
+(defn create-grid-data [ncol nrow genfn]
+  (vec (for [y (range nrow)]
+         (vec (for [x (range ncol)]
                 (genfn (pos/create x y)))))))
 
 (defn create [ncol nrow genfn]
   {:ncol ncol
    :nrow nrow
-   :data (create-terrain-vec ncol nrow genfn)})
+   :data (create-grid-data ncol nrow genfn)})
 
-(defn in-bounds? [m pos]
-  (let [x (:x pos)
-        y (:y pos)
-        ncol (:ncol m)
-        nrow (:nrow m)]
+(defn in-bounds? [g pos]
+  (let [x (pos/get-x pos)
+        y (pos/get-y pos)
+        ncol (:ncol g)
+        nrow (:nrow g)]
     (and (>= x 0) (>= y 0) (< x ncol) (< y nrow))))
 
-(defn get-val [m pos]
-  (let [x (:x pos)
-        y (:y pos)
-        data (:data m)
+(defn get-val [g pos]
+  (let [x (pos/get-x pos)
+        y (pos/get-y pos)
+        data (:data g)
         row (get data y)]
+    ;(println "Getting at x=" x ", y=" y ",pos=" pos ", result=" (get row x))
     (get row x)))
